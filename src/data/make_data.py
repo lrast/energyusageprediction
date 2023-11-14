494c6588a17fd624666958024a6c64061ff352cd
luke <luke.rast@gmail.com>
2023-11-14 00:31:00 -0500

    taking care of timezones

--- src/data/make_data.py
+++ src/data/make_data.py
@@ -6,46 +6,22 @@ import datetime
 
 def clean_data():
     """missing data imputation and duplicate removal"""
-    shutil.copy('data/raw/client.csv', 'data/processed/client.csv')
     print('client')
+    shutil.copy('data/raw/client.csv', 'data/processed/client.csv')
 
-    # impute missing data for electricity prices
+    # convert to UTC and impute missing data for electricity prices
+    print('electricity_prices')
     electricityPrices = pd.read_csv('data/raw/electricity_prices.csv')
-    forecastTimes = pd.to_datetime(electricityPrices.forecast_date).sort_values()
-    fullDateRange = pd.date_range(forecastTimes.min(), forecastTimes.max(),
-                                  freq='H')
-    missingForecastTimes = fullDateRange.difference(forecastTimes)
-
-    for missing in missingForecastTimes:
-        prevInd = (missing + datetime.timedelta(hours=-1)
-                   ).strftime("%Y-%m-%d %H:%M:%S")
-        nextInd = (missing + datetime.timedelta(hours=1)
-                   ).strftime("%Y-%m-%d %H:%M:%S")
-
-        ForecastDate = missing.strftime("%Y-%m-%d %H:%M:%S")
-        OriginDate = (missing + datetime.timedelta(days=-1)
-                      ).strftime("%Y-%m-%d %H:%M:%S")
-        BlockId = electricityPrices.data_block_id.max() + 1
-        Price = (electricityPrices[electricityPrices.forecast_date == prevInd]
-                                  .euros_per_mwh.item() +
-                 electricityPrices[electricityPrices.forecast_date == nextInd]
-                                  .euros_per_mwh.item()
-                 )/2
-
-        electricityPrices.loc[len(electricityPrices)] = {
-                                'forecast_date': ForecastDate, 
-                                'euros_per_mwh': Price, 
-                                'origin_date': OriginDate, 
-                                'data_block_id': BlockId
-                                }
-
-    electricityPrices = electricityPrices.sort_values('forecast_date')
+    electricityPrices = localToUTC(electricityPrices, 'forecast_date', 'Europe/Warsaw')
+    electricityPrices.origin_date = electricityPrices.forecast_date - \
+                                        datetime.timedelta(days=1)
+    electricityPrices.sort_values('forecast_date')
 
     electricityPrices.to_csv('data/processed/electricity_prices.csv', index=False)
-    print('electricity_prices')
 
     # impute missing data for weather forcast
     # it happens that these are the same date and location
+    print('forecast_weather')
     weatherForecast = pd.read_csv('data/raw/forecast_weather.csv')
     subSelection = weatherForecast[(weatherForecast.latitude == 59.7) & 
                 (weatherForecast.longitude == 23.7) & 
@@ -57,16 +33,39 @@ def clean_data():
         )/2
     weatherForecast.fillna(fillVal)
     weatherForecast.to_csv('data/processed/forecast_weather.csv', index=False)
-    print('forecast_weather')
 
-    shutil.copy('data/raw/gas_prices.csv', 'data/processed/gas_prices.csv')
     print('gas_prices')
+    shutil.copy('data/raw/gas_prices.csv', 'data/processed/gas_prices.csv')
 
     # remove duplicates from historical weather
+    print('historical_weather')
     weatherHistorical = pd.read_csv('data/raw/historical_weather.csv')
     weatherHistorical.drop_duplicates()
+    weatherHistorical.datetime = pd.to_datetime(weatherHistorical.datetime
+                                                ).dt.tz_localize('UTC')
     weatherHistorical.to_csv('data/processed/historical_weather.csv', index=False)
-    print('historical_weather')
 
-    shutil.copy('data/raw/train.csv', 'data/processed/train.csv')
     print('train')
+    trainData = pd.read_csv('data/raw/train.csv')
+    # note the strange behavior of gmt -2, which converts to utc+2...
+    trainData.datetime = pd.to_datetime(trainData.datetime). \
+                            dt.tz_localize('Etc/GMT-2').dt.tz_convert('UTC')
+    trainData.to_csv('data/processed/train.csv', index=False)
+
+
+def localToUTC(df, timeInd, location):
+    """Converts local time to UTC, and adds missing hours"""
+    localTime = pd.to_datetime(df[timeInd]).dt.tz_localize(location, ambiguous=True)
+    df[timeInd] = localTime.dt.tz_convert('UTC')
+
+    allTimes = pd.date_range(df[timeInd].min(), df[timeInd].max(), freq='H')
+    missingTimes = allTimes.difference(df[timeInd])
+
+    for missing in missingTimes:
+        prev = missing + datetime.timedelta(hours=-1)
+        fillData = df[df[timeInd] == prev].iloc[-1].to_dict()
+        fillData[timeInd] = missing
+
+        df.loc[len(df)] = fillData
+
+    return df
